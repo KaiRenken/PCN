0e16124f6a88f7dd465b60e43774a5e9d49576ef
Kai Renken <s_n5rv8u@agkozlov07.math.uni-bremen.de>
2018-02-06 13:55:29 +0100

    Several changes (README.md written, displayHowTo changed, .gitignore changed, documentation added)

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-Readme information coming soon.
+PCN - Partitioning Consecutive Numbers (by Kai Renken, 2018)
+
+
+- What the program does -
+
+PCN partitiones natural numbers from 1 to n, which add up to the same sum as some natural numbers from a to b, such that each part of the partition adds up to exactly one of the numbers between a and b. Further information about the mathematical background can be found in 'docs/A theorem about partitioning consecutive numbers.pdf'.
+
+
+- How to use -
+
+PCN is tested to compile with the GCC and MinGW compilers. Just place all files from 'src' in one folder and compile the 'PCN.cpp'. Executable files for Windows and Linux can be found in 'build'.
+
+Run PCN in command line as follows:
+
+'PCN [Parameter n] [Parameter a] [Parameter b]'
+
+Here, n, a and b as supposed to be natural numbers, such that the sum from 1 to n equals the sum from a to b. The output will be a matrix consisting of b-a+1 lines and n columns, such that the entries in the first line add up to a, the entries in the second line add up to a+1, and so on.
+
+
+- Tests -
+
+PCN has been tested using the Catch2 framework (see: 'https://github.com/catchorg/Catch2')
+You can run the tests by placing all files from 'test' in one folder and compile the 'test.cpp'.
--- test/basics.h
+++ test/basics.h
@@ -59,12 +59,6 @@ int findSecondFreeSpace(int pSteps, int pShift) {
 void displayHowTo() {
     std::cout << "PCN - Partitioning Consecutive Numbers (written by Kai Renken, 2018)" << std::endl;
     std::cout << std::endl;
-    std::cout << "Use 'PCN [Parameter n] [Parameter a] [Parameter b]'" << std::endl;
-    std::cout << "Here n, a, and b should be natural numbers," << std::endl << "such that the sum of the numbers from 1 to n" << std::endl << "equals the sum of the numbers from a to b." << std::endl;
-    std::cout << "PCN will partition the numbers from 1 to n, such that" << std::endl << "the numbers in each part add up to exactly one number between a and b." << std::endl;
-    std::cout << "The output will be b-a+1 lines with n entries, such that" << std::endl << "the numbers in the first line add up to a, the numbers in the" << std::endl << "second line add up to a+1, and so on." << std::endl;
-    std::cout << std::endl;
-    std::cout << "For example enter 'PCN 5 7 8' and the output will consist" << std::endl << "of 2 lines, since there are 2 numbers between 7 and 8." << std::endl << "The first line will consist of the numbers 4 and 3," << std::endl << "since they add up to 7 and the second line will consist of" << std::endl << "the numbers 5, 2 and 1, since they add up to 8." << std::endl;
-    std::cout << std::endl;
-    std::cout << "Of course there are more possibilities to partition these numbers" << std::endl << "satisfying the same properties, but until now this is the only" << std::endl << "known general algorithm to realize that partitioning." << std::endl;
+    std::cout << "Type 'PCN [Parameter n] [Parameter a] [Parameter b]'," << std::endl;
+    std::cout << "such that the sum of the numbers 1 to n equals the sum of the numbers a to b" << std::endl;
 }
